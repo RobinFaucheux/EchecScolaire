684a7d2d467a7356d4dd5c68ed50a64102a7274b
RobinFaucheux <robin.faucheux@etu.univ-orleans.fr>
2025-12-14 00:33:55 +0100

    fixed the diagonal movement of pawns

--- src/model/Pieces/Pawn.py
+++ src/model/Pieces/Pawn.py
@@ -21,3 +21,18 @@ class Pawn(Piece):
         if self.color == Color.WHITE and self.case.get_pos()[0] != 1 and (2, 0) in self._vectors:
             self._vectors.remove((2, 0))
         return super().process_vectors()
+    
+    def remove_lines_after_piece(self):
+        l = super().remove_lines_after_piece()
+        res = [l[1]]
+        piece_1 = self.case.get_board().get_case(l[0]).get_piece()
+        if piece_1 is not None and piece_1.get_color() != self.color:
+            res.append(l[0])
+        
+        piece_2 = self.case.get_board().get_case(l[2]).get_piece()
+        if piece_2 is not None and piece_2.get_color() != self.color:
+            res.append(l[2])
+        
+        if (len(l) >3):
+            res.append(l[-1])
+        return res
